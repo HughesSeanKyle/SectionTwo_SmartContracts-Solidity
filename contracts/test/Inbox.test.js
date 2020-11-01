d1d07981b7d72144d8332e78b75f32b6af1428c3
HughesSeanKyle <khughessean@yahoo.com>
2020-11-01 11:56:32 +0200

    V41 - Web3 Providers

--- contracts/test/Inbox.test.js
+++ contracts/test/Inbox.test.js
@@ -6,3 +6,26 @@ const ganache = require('ganache-cli');
 //Constructor used to create instances of web3 lib
   //instance of Web3 will be a lowercase w - convention.
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+1. One instance of web3 connects to the particular test or main network you wich to connect to. 
+2. Provider - Communitation layer between Web3 lib and some specific ethereum network.
+*/
