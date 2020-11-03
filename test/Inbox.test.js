312c9359dd71a5ead3f6f34ffc160134032bd54e
HughesSeanKyle <khughessean@yahoo.com>
2020-11-03 16:42:05 +0200

    V45 - Refactor to Async/Await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,13 +8,11 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+let accounts;
 
-beforeEach(() => {
+beforeEach(async () => {
   //Get a list of all accounts
-  web3.eth.getAccounts()
-  .then(fetchedAccounts => {
-    console.log(fetchedAccounts);
-  });
+  accounts = await web3.eth.getAccounts();
 
   //use one of these accounts to deploy contract
 
@@ -22,7 +20,7 @@ beforeEach(() => {
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-
+    console.log(accounts);
   });
 });
 
