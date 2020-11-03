6111263bb6fef97a10613a6fab543233f4832b12
HughesSeanKyle <khughessean@yahoo.com>
2020-11-03 16:34:22 +0200

    V44 - Fetching Accounts from Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,36 +8,24 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-//-------------------Example test with Mocha--------------------
-class Car {
-  park() {
-    return 'stopped';
-  }
 
-  drive() {
-    return 'vroom';
-  }
-}
-
-let car;
-
-//Logic will be executed before any logic in it stmt
-  //Runs once, then it, runs again then 2nd it
 beforeEach(() => {
-  car = new Car();
-});
-
-//describe will group together it stmts
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
+  //Get a list of all accounts
+  web3.eth.getAccounts()
+  .then(fetchedAccounts => {
+    console.log(fetchedAccounts);
   });
 
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
+  //use one of these accounts to deploy contract
+
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+
   });
 });
-//-------------------Example test with Mocha--------------------
+
 
 
 
@@ -57,4 +45,5 @@ describe('Car', () => {
 1. One instance of web3 connects to the particular test or main network you wich to connect to. 
 2. Provider - Communitation layer between Web3 lib and some specific ethereum network.
 3. The assert module provides a way of testing expressions. If the expression evaluates to 0, or false, an assertion failure is being caused, and the program is terminated. This module was built to be used internally by Node.
+4. Almost every function that is called with web3 is asynchronous which means it will return a promise.  
 */
