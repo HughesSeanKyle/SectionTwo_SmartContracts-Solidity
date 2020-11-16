15df2af1958548782bf2c036a5c02acc8d78afbe
HughesSeanKyle <khughessean@yahoo.com>
2020-11-16 11:19:11 +0200

    Wallet Provider Setup

---
+++ deploy.js
@@ -0,0 +1,12 @@
+// V54 - Wallet Provider Setup
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const keys = require('./config/keys');
+
+const provider = new HDWalletProvider(
+    keys.metaMaskMnemonic,
+    keys.InfuraLink
+);
+const web3 = new Web3(provider);
